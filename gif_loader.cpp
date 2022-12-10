af9ba66816da1778af0966a088b411e11c112f30
Relintai <relintai@protonmail.com>
2022-12-10 17:14:50 +0100

    Gif loader api setup.

--- gif_loader.cpp
+++ gif_loader.cpp
@@ -1,2 +1,17 @@
 
 #include "gif_loader.h"
+
+Array GIFLoader::load_gif(const String &file) {
+	Array images;
+
+	return images;
+}
+
+GIFLoader::GIFLoader() {
+}
+GIFLoader::~GIFLoader() {
+}
+
+void GIFLoader::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("load_gif", "file"), &GIFLoader::load_gif);
+}
--- gif_loader.h
+++ gif_loader.h
@@ -4,8 +4,19 @@
 
 #include "core/object/reference.h"
 
+#include "core/io/image.h"
+
 class GIFLoader : public Reference {
+    GDCLASS(GIFLoader, Reference);
+
 public:
+	Array load_gif(const String &file);
+
+	GIFLoader();
+	~GIFLoader();
+
+private:
+	static void _bind_methods();
 };
 
 #endif 
